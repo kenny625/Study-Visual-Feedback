61b7577667754cc9ad178361e26a4a427747048b
WeiChen <kenny625@gmail.com>
2014-03-11 16:40:00 +0800

    finish Voronoi click

--- keyboard.js
+++ keyboard.js
@@ -27,16 +27,13 @@ canvas.addEventListener('click', function (event) {
         drawPoint(mousePos.x, mousePos.y);
         IFimg.startX = mousePos.x;
         IFimg.startY = mousePos.y;
-        console.log("start  " + IFimg.startX + " " + IFimg.startY);
         currentKey = 0;
     } else if (currentKey == 50) {
         drawPoint(mousePos.x, mousePos.y);
         IFimg.endX = mousePos.x;
         IFimg.endY = mousePos.y;
         currentKey = 0;
-    } else if (currentKey == 52) {
-
-    }
+    } 
 
 
 }, false);
@@ -75,6 +72,15 @@ document.onkeydown = function () {
         ctx.fillStyle = "#00FF00";
     ctx.fillRect(470, 140, 8, 8);
     ctx.fillRect(870, 140, 8, 8);
+    }else if (currentKey == 52){
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            var VoronoiCanvas = document.createElement("canvas");
+            VoronoiCanvas.setAttribute("id", "voronoiCanvas");
+            VoronoiCanvas.setAttribute("style", "cursor:crosshair");
+            VoronoiCanvas.setAttribute("width", "980");
+            VoronoiCanvas.setAttribute("height", "551");
+            insertAfter(document.getElementById("myCanvas"), VoronoiCanvas);
+            Voronoi.init();
     }
 }
 
@@ -109,3 +115,152 @@ function lineDistance(x1, y1, x2, y2) {
 
     return Math.sqrt(xs + ys);
 }
+
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+
+
+
+var Voronoi = {
+	voronoi: new Voronoi(),
+	sites: [],
+	diagram: null,
+	margin: 100,
+	canvas: null,
+	bbox: {xl:0,xr:980,yt:0,yb:551},
+
+	normalizeEventCoords: function(target,e) {
+		// http://www.quirksmode.org/js/events_properties.html#position
+		// =====
+		if (!e) {e=self.event;}
+		var x = 0;
+		var y = 0;
+		if (e.pageX || e.pageY) {
+			x = e.pageX;
+			y = e.pageY;
+			}
+		else if (e.clientX || e.clientY) {
+			x = e.clientX+document.body.scrollLeft+document.documentElement.scrollLeft;
+			y = e.clientY+document.body.scrollTop+document.documentElement.scrollTop;
+			}
+		// =====
+		return {x:x-target.offsetLeft,y:y-target.offsetTop};
+		},
+
+	init: function() {
+		var me = this;
+		this.canvas = document.getElementById('voronoiCanvas');
+//		this.canvas.onmousemove = function(e) {
+//			if (!me.sites.length) {return;}
+//			var site = me.sites[0];
+//			var mouse = me.normalizeEventCoords(me.canvas,e);
+//			site.x = mouse.x;
+//			site.y = mouse.y;
+//			me.diagram = me.voronoi.compute(me.sites,me.bbox);
+//			me.render();
+//			};
+		this.canvas.onclick = function(e) {
+			var mouse = me.normalizeEventCoords(me.canvas,e);
+			me.addSite(mouse.x,mouse.y);
+			me.render();
+			};
+//		this.randomSites(10,true);
+//        me.addSite(100,100);
+//        me.addSite(200,200);
+//        me.addSite(300,300);
+		this.render();
+		},
+
+	clearSites: function() {
+		// we want at least one site, the one tracking the mouse
+		this.sites = [{x:0,y:0}];
+		this.diagram = this.voronoi.compute(this.sites, this.bbox);
+		},
+
+	randomSites: function(n,clear) {
+		if (clear) {this.sites = [];}
+		var xo = this.margin;
+		var dx = this.canvas.width-this.margin*2;
+		var yo = this.margin;
+		var dy = this.canvas.height-this.margin*2;
+		for (var i=0; i<n; i++) {
+			this.sites.push({x:self.Math.round(xo+self.Math.random()*dx),y:self.Math.round(yo+self.Math.random()*dy)});
+			}
+		this.diagram = this.voronoi.compute(this.sites, this.bbox);
+		},
+
+	addSite: function(x,y) {
+		this.sites.push({x:x,y:y});
+		this.diagram = this.voronoi.compute(this.sites, this.bbox);
+		},
+
+	render: function() {
+		var ctx = this.canvas.getContext('2d');
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		// background
+		ctx.globalAlpha = 0.5;
+//		ctx.beginPath();
+//		ctx.rect(0,0,this.canvas.width,this.canvas.height);
+//		ctx.fillStyle = '#fff';
+//		ctx.fill();
+//		ctx.strokeStyle = '#888';
+//		ctx.stroke();
+		// voronoi
+		if (!this.diagram) {return;}
+		ctx.strokeStyle='#000';
+		// edges
+		var edges = this.diagram.edges,
+			nEdges = edges.length,
+			v;
+		if (nEdges) {
+			var edge;
+			ctx.beginPath();
+			while (nEdges--) {
+				edge = edges[nEdges];
+				v = edge.va;
+				ctx.moveTo(v.x,v.y);
+				v = edge.vb;
+				ctx.lineTo(v.x,v.y);
+				}
+			ctx.stroke();
+			}
+		// how many sites do we have?
+		var sites = this.sites,
+			nSites = sites.length;
+		if (!nSites) {return;}
+		// highlight cell under mouse
+		var cell = this.diagram.cells[this.sites[0].voronoiId];
+		// there is no guarantee a Voronoi cell will exist for any
+		// particular site
+		if (cell) {
+			var halfedges = cell.halfedges,
+				nHalfedges = halfedges.length;
+			if (nHalfedges > 2) {
+				v = halfedges[0].getStartpoint();
+				ctx.beginPath();
+				ctx.moveTo(v.x,v.y);
+				for (var iHalfedge=0; iHalfedge<nHalfedges; iHalfedge++) {
+					v = halfedges[iHalfedge].getEndpoint();
+					ctx.lineTo(v.x,v.y);
+					}
+				ctx.fillStyle = '#faa';
+				ctx.fill();
+				}
+			}
+		// draw sites
+		var site;
+		ctx.beginPath();
+		ctx.fillStyle = '#44f';
+		while (nSites--) {
+			site = sites[nSites];
+			ctx.rect(site.x-2/3,site.y-2/3,2,2);
+            ctx.fillStyle = "#FF0000";
+    ctx.fillRect(site.x - 4, site.y - 4, 8, 8);
+			}
+		ctx.fill();
+		},
+	};
+
+
