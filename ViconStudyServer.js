dd926512cd0b48d91c6824f0491f4cd31b26cfac
WeiChen <kenny625@gmail.com>
2014-04-03 15:20:16 +0800

    catch error

--- ViconStudyServer.js
+++ ViconStudyServer.js
@@ -20,49 +20,55 @@ var userName;
 wss.on('connection', function (ws) {
     ws.on('message', function (message) {
         console.log('received: %s', message);
-        var message_obj = JSON.parse(message);
-        switch (message_obj.action) {
-        case "saveLayout":
-            savedLayout = message_obj.layout;
-            writeToFile(message);
-            break;
-        case "setHand":
-            var layoutObj = new Object();
-            try {
-                layoutObj = JSON.parse(fs.readFileSync(filepath + userName + ".txt").toString());
-            } catch (e) {
-                console.log(e);
+        try {
+            var message_obj = JSON.parse(message);
+            switch (message_obj.action) {
+            case "saveLayout":
+                savedLayout = message_obj.layout;
+                writeToFile(message);
+                break;
+            case "setHand":
+                var layoutObj = new Object();
+                try {
+                    layoutObj = JSON.parse(fs.readFileSync(filepath + userName + ".txt").toString());
+                } catch (e) {
+                    console.log(e);
+                }
+                layoutObj.action = "setHand";
+                wss.broadcast(JSON.stringify(layoutObj));
+                //                ws.send(JSON.stringify(layoutObj));
+                break;
+            case "loadLayout":
+                var layoutObj = new Object();
+                try {
+                    layoutObj = JSON.parse(fs.readFileSync(filepath + userName + ".txt").toString());
+                } catch (e) {
+                    console.log(e);
+                }
+                layoutObj.action = "loadLayout";
+                wss.broadcast(JSON.stringify(layoutObj));
+                //                ws.send(JSON.stringify(layoutObj));
+                break;
+            case "setName":
+                userName = message_obj.userName;
+                console.log(userName);
+                break;
+            case "ViconData":
+                wss.broadcast(message);
+                break;
+            case "dumpVertices":
+                wss.broadcast(message);
+                break;
+            case "sentence":
+                wss.broadcast(message);
+                break;
+            default:
             }
-            layoutObj.action = "setHand";
-            wss.broadcast(JSON.stringify(layoutObj));
-            //                ws.send(JSON.stringify(layoutObj));
-            break;
-        case "loadLayout":
-            var layoutObj = new Object();
-            try {
-                layoutObj = JSON.parse(fs.readFileSync(filepath + userName + ".txt").toString());
-            } catch (e) {
-                console.log(e);
-            }
-            layoutObj.action = "loadLayout";
-            wss.broadcast(JSON.stringify(layoutObj));
-            //                ws.send(JSON.stringify(layoutObj));
-            break;
-        case "setName":
-            userName = message_obj.userName;
-            console.log(userName);
-            break;
-        case "ViconData":
-            wss.broadcast(message);
-            break;
-        case "dumpVertices":
-            wss.broadcast(message);
-            break;
-        case "sentence":
-            wss.broadcast(message);
-            break;
-        default:
+        } catch (err) {
+            // handle the error safely
+            console.log(err);
         }
+
     });
 
 });
